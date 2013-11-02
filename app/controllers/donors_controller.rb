ef3a97d21654ef3d8ad4a4de63b0a6905685fbc9
Stephen J. Weaver <sjwsgm@gmail.com>
2013-11-02 10:08:26 -0500

    almost can update profiles

--- app/controllers/donors_controller.rb
+++ app/controllers/donors_controller.rb
@@ -1,4 +1,6 @@
 class DonorsController < ApplicationController
+   before_action :set_donor, only: [:show, :edit, :update, :destroy]
+
    def create
       if current_user && !current_user.has_profile?
          current_user.profile = Donor.create!
@@ -9,7 +11,20 @@ class DonorsController < ApplicationController
    end
 
    def index
-      @donor = Donor.where(id: current_user.user.rolable_id)
+      @donors = Donor.all
    end
 
+   def show
+      
+   end
+
+   private
+
+      def set_donor
+         @donor = Donor.find(params[:id])
+      end
+
+      def donor_params
+         params.require(:donor).permit(:first_name, :last_name, user: [:profile, :email])
+      end
 end
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,27 +1,32 @@
 class RegistrationsController < Devise::RegistrationsController
-  before_filter :user_params, only: [:create, :edit]
+  before_filter :user_params, only: [:create, :edit, :update]
   before_filter :authenticate_user!, only: :edit
 
   def create
     # Deleting the user_type from the params hash, won't work without this.
+    Rails.logger.info("@user_type")
+    Rails.logger.info(@user_type)
     params[:user].delete(:user_type)
     build_resource(sign_up_params)
 
     # crate a new child instance depending on the given user user_type  
     child_class = @user_type.camelize.constantize
-    resource.rolable = child_class.create(user_params)
+
+    resource.rolable = child_class.create(params[@user_type])
 
     # first check if child instance is valid
     # cause if so and the parent instance is valid as well
     # it's all being saved at once
     valid = resource.valid?
     valid = resource.rolable.valid? && valid
-    # customized code end
 
-    if resource.save
+    # customized code end
+    if resource.save && resource.rolable.save
+      Rails.logger.info("Sucess")
       if resource.active_for_authentication?
         set_flash_message :notice, :signed_up if is_navigational_format?
         sign_up(resource_name, resource)
+
         respond_with resource, :location => after_sign_up_path_for(resource)
       else
         set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_navigational_format?
@@ -29,6 +34,7 @@ class RegistrationsController < Devise::RegistrationsController
         respond_with resource, :location => after_inactive_sign_up_path_for(resource)
       end
     else
+      Rails.logger.info("FAILED")
       clean_up_passwords resource
       respond_to do |format|
         format.html { render :text => resource.errors.messages}
@@ -38,66 +44,82 @@ class RegistrationsController < Devise::RegistrationsController
 
 
    def update
-    # Deleting the user_type from the params hash, won't work without this.
-    current_user.user.update(params[:user].permit(:email, :password, :password_confirmation, :remember_me, :provider, :uid, :profile))
-    current_user.update(user_params)
-    if  current_user.user.valid? && current_user.valid?
-        current_user.user.save!
-        current_user.save!
-        set_flash_message :notice, "Your user acount has been updated"
-        respond_with resource, :location => after_sign_up_path_for(resource.user)
-    else
-      set_flash_message :error, "Your account could not be updated"
-      respond_to do |format|
-        format.html { render :text => current_user.errors.messages}
+    self.resource = @user_type.camelize.constantize.to_adapter.get!(send(:"current_#{resource_name}").to_key)
+    prev_unconfirmed_email = resource.unconfirmed_email if resource.respond_to?(:unconfirmed_email)
+
+    if resource.update_attributes(params[@user_type.humanize]) && resource.user.update_attributes(params[:user][:user])
+      if is_navigational_format?
+        flash_key = update_needs_confirmation?(resource.user, prev_unconfirmed_email) ?
+          :update_needs_confirmation : :updated
+        set_flash_message :notice, flash_key
       end
+      sign_in resource_name, resource.user, :bypass => true
+      respond_with resource, :location => after_update_path_for(resource)
+    else
+      clean_up_passwords resource
+      respond_with resource
     end
+  #   # Deleting the user_type from the params hash, won't work without this.
+
+  #   current_user.user.update_attributes(params[:user][:user].permit(:email, :password, :password_confirmation, :remember_me, :provider, :uid, :profile))
+  #   current_user.update_attributes(params[@user_type])
+  #   if  current_user.user.valid? && current_user.valid?
+  #       current_user.user.save!
+  #       current_user.save!
+  #       set_flash_message :notice, "Your user acount has been updated"
+  #       respond_with resource, :location => after_sign_up_path_for(resource.user)
+  #   else
+  #     raise
+  #     set_flash_message :error, "Your account could not be updated"
+  #     respond_to do |format|
+  #       format.html { render :text => current_user.errors.messages}
+  #     end
+  #   end
   end
 
 
 
-private
-  def user_params
-    if params[:user].nil? && !@current_user.nil?
-      if !@current_user.rolable_type.nil?
-        @user_type = @current_user.rolable_type.downcase
-      else 
-        @user_type = "donor"
+  private
+    def user_params
+      if params[:user].nil? && !@current_user.nil?
+        if !@current_user.rolable_type.nil?
+          @user_type = @current_user.rolable_type.downcase
+        else 
+          @user_type = "donor"
+        end
+      elsif !params[:user].nil? && @current_user.nil?
+        @user_type = params[:user][:user_type]
+      elsif !params[:user].nil? && !current_user.nil?
+        @user_type = current_user.user.rolable_type.underscore
+      else
+        render root_path
       end
-    elsif !params[:user].nil? && @current_user.nil?
-      @user_type = params[:user][:user_type]
-    elsif !params[:user].nil? && !current_user.nil?
-      @user_type = current_user.user.rolable_type.underscore
-    else
-      render root_path
-    end
 
-    begin
+
       if !params[:user].nil? && !current_user.nil? 
+        params[:user][:user] = params[:user][:user].permit(:email, :password, :password_confirmation, :remember_me, :provider, :uid, :profile)
         case @user_type
           when "charity"
             params[:Charity].permit(:name, :eid, :description)
           when "business"
              params[:Business].permit(:name, :goods, :description, :services)
           when "donor"
-             params[:Donor].permit(:title, :first_name, :last_name, :middle_initial)
+             params[:Donor] = params[:Donor].permit(:title, :first_name, :last_name, :middle_initial)
         end
       elsif !params[:user].nil?
         case @user_type
           when "charity"
-            params.require(:charity).permit(:name, :eid, :description)
+            params[:charity] = params[:charity].permit(:name, :eid, :description)
           when "business"
-            params.require(:business).permit(:name, :goods, :description, :services)
+            params[:business]= params[:business].permit(:name, :goods, :description, :services)
           when "donor"
-             params.require(:donor).permit(:title, :first_name, :last_name, :middle_initial)
+            params[:donor] = params[:donor].permit(:title, :first_name, :last_name, :middle_initial)
         end
       elsif !current_user.nil?
         @user_type = current_user.user.rolable_type.underscore
       else
-        @user_type = ""
+        @user_type = "nbgvfd"
       end
-    rescue
-      @user_type = ""
     end
-  end
+
 end
