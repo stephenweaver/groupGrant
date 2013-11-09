d9fcafce313ac8443fa576019d6a2b3b60d4520b
Gordon <g.badgett3@gmail.com>
2013-11-09 07:40:35 -0600

    changes to groupGrant pages, added photos to groupGrant table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131108082034) do
+ActiveRecord::Schema.define(version: 20131109051451) do
 
   create_table "addresses", force: true do |t|
     t.integer  "user_id"
@@ -85,6 +85,10 @@ ActiveRecord::Schema.define(version: 20131108082034) do
     t.string   "video_url"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "groupgrant_pic_file_name"
+    t.string   "groupgrant_pic_content_type"
+    t.integer  "groupgrant_pic_file_size"
+    t.datetime "groupgrant_pic_updated_at"
   end
 
   create_table "users", force: true do |t|
