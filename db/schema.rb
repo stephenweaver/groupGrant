b6c683c66e1d5efd470a5aefb553d9d6adcc1243
Stephen J. Weaver <sjwsgm@gmail.com>
2013-11-09 10:34:00 -0600

    add groupgrants to nav bar

--- db/schema.rb
+++ db/schema.rb
@@ -92,12 +92,12 @@ ActiveRecord::Schema.define(version: 20131109051451) do
   end
 
   create_table "users", force: true do |t|
-    t.string   "email",                     default: "", null: false
-    t.string   "encrypted_password",        default: "", null: false
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",             default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -106,8 +106,6 @@ ActiveRecord::Schema.define(version: 20131109051451) do
     t.datetime "updated_at"
     t.string   "provider"
     t.string   "uid"
-    t.string   "fb_token_expires_at_token"
-    t.string   "fb_token_expires_at"
     t.integer  "rolable_id"
     t.string   "rolable_type"
     t.string   "profile_file_name"
