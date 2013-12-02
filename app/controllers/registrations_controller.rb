92e01701c691a2a33d68a51674f7203c0e46ab2e
Stephen J. Weaver <sjwsgm@gmail.com>
2013-12-02 12:08:25 -0600

    Home page and forms and smaller view

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -26,7 +26,7 @@ class RegistrationsController < Devise::RegistrationsController
         set_flash_message :notice, :signed_up if is_navigational_format?
         sign_up(resource_name, resource)
 
-        respond_with resource, :location => after_sign_up_path_for(resource)
+        respond_with resource.rolable
       else
         set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_navigational_format?
         expire_session_data_after_sign_in!
