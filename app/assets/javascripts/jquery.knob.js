5546fc9a23733600585c9518881e0e569fed4b71
Gordon <gbadge5789@students.pcci.edu>
2014-01-27 17:22:22 -0600

    formatting for gG page and progress bar

--- app/assets/javascripts/jquery.knob.js
+++ app/assets/javascripts/jquery.knob.js
@@ -114,7 +114,8 @@
                     step : this.$.data('step') || 1,
 
                     // Hooks
-                    draw : null, // function () {}
+                    draw : null, //function () {}
+
                     change : null, // function (value) {}
                     cancel : null, // function () {}
                     release : null // function (value) {}
@@ -776,14 +777,55 @@
 
 $(function() {
     $(".dial").knob({
-      'draw' : function () { 
-        $(this.i).val(this.cv + '%')
+      'draw' : function () {
+
+    $(this.i).val(this.cv + '%')
+      // "tron" case
+      if(this.$.data('skin') == 'tron') {
+
+            var a = this.angle(this.cv)  // Angle
+                , sa = this.startAngle          // Previous start angle
+                , sat = this.startAngle         // Start angle
+                , ea                            // Previous end angle
+                , eat = sat + a                 // End angle
+                , r = 1;
+
+            this.g.lineWidth = this.lineWidth;
+
+            this.o.cursor
+                && (sat = eat - 0.3)
+                && (eat = eat + 0.3);
+
+            if (this.o.displayPrevious) {
+                ea = this.startAngle + this.angle(this.v);
+                this.o.cursor
+                    && (sa = ea - 0.3)
+                    && (ea = ea + 0.3);
+                this.g.beginPath();
+                this.g.strokeStyle = this.pColor;
+                this.g.arc(this.xy, this.xy, this.radius - this.lineWidth, sa, ea, false);
+                this.g.stroke();
+            }
+
+            this.g.beginPath();
+            this.g.strokeStyle = r ? this.o.fgColor : this.fgColor ;
+            this.g.arc(this.xy, this.xy, this.radius - this.lineWidth, sat, eat, false);
+            this.g.stroke();
+
+            this.g.lineWidth = 2;
+            this.g.beginPath();
+            this.g.strokeStyle = this.o.fgColor;
+            this.g.arc( this.xy, this.xy, this.radius - this.lineWidth + 1 + this.lineWidth * 2 / 3, 0, 2 * Math.PI, false);
+            this.g.stroke();
+
+            return false;
+        }
       }
     })
     .trigger('configure',
             {
-                "fgColor":   "#000066",
-                "inputColor":"#000066",
+                "inputColor":"#003399",
+                "color":"#FFF",
             }
     );
 })
