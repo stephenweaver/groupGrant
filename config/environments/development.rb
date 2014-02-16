32900d1058ff2ea31f61d7ca7730b8601d8d3f8e
Stephen J. Weaver <sjwsgm@gmail.com>
2014-02-16 15:14:29 -0600

    re-formate gg page

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -25,7 +25,8 @@ Blankcomposer::Application.configure do
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
-  config.assets.debug = true
+  config.assets.debug = false
+
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE 
 end
