37771847a7dc52dc3cb0ef9207e64dba68b7222b
Joshua <kwodom1512@students.pcci.edu>
2014-02-22 23:50:55 -0600

    fixed some formating things and enabled renderknob()

--- app/assets/javascripts/groupgrants.js
+++ app/assets/javascripts/groupgrants.js
@@ -23,8 +23,6 @@ sendRequest = function() {
 		    // whether this is a POST or GET request
 		    type: "POST",
 		 
-		    // the type of data we expect back
-		    
 		    // code to run if the request succeeds;
 		    // the response is passed to the function
 		    success: function( data) {
@@ -40,7 +38,6 @@ sendRequest = function() {
 		    error: function(data) {
         		alert("error " + data.name );
    			}
-
 		});
 	});
 };
@@ -60,8 +57,6 @@ cancelRequest = function() {
 		    },
 		    // whether this is a POST or GET request
 		    type: "POST",
-		 
-		    // the type of data we expect back
 		    
 		    // code to run if the request succeeds;
 		    // the response is passed to the function
@@ -82,13 +77,12 @@ cancelRequest = function() {
 
 $(window).load(function() {
    
-   //renderKnob();
+   renderKnob();
    sendRequest();
-   cancelRequest();
-   console.log("koffiTest() loaded");
+   cancelRequest();   
 });
 
-//$(document).on('page:load', renderKnob);
+$(document).on('page:load', renderKnob);
 $(document).on('page:load', sendRequest);
 $(document).on('page:load', cancelRequest);
 
