b30cd79ecf74f315662a65f95775a5fe290926a0
Joshua <kwodom1512@students.pcci.edu>
2014-03-08 13:50:38 -0600

    resolving conflicts

--- db/schema.rb
+++ db/schema.rb
@@ -233,6 +233,7 @@ ActiveRecord::Schema.define(version: 20140307011946) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
   add_index "spree_configurations", ["name", "type"], name: "index_spree_configurations_on_name_and_type", using: :btree
 
   create_table "spree_countries", force: true do |t|
@@ -826,12 +827,12 @@ ActiveRecord::Schema.define(version: 20140307011946) do
   end
 
   create_table "users", force: true do |t|
-    t.string   "email",                             default: "", null: false
-    t.string   "encrypted_password",                default: "", null: false
+    t.string   "email",                                default: "", null: false
+    t.string   "encrypted_password",                   default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                     default: 0,  null: false
+    t.integer  "sign_in_count",                        default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -840,6 +841,8 @@ ActiveRecord::Schema.define(version: 20140307011946) do
     t.datetime "updated_at"
     t.string   "provider"
     t.string   "uid"
+    t.string   "fb_token_expires_at_token"
+    t.string   "fb_token_expires_at"
     t.integer  "rolable_id"
     t.string   "rolable_type"
     t.string   "profile_file_name"
@@ -847,7 +850,7 @@ ActiveRecord::Schema.define(version: 20140307011946) do
     t.integer  "profile_file_size"
     t.datetime "profile_updated_at"
     t.string   "phone"
-    t.string   "spree_api_key",          limit: 48
+    t.string   "spree_api_key",             limit: 48
     t.integer  "ship_address_id"
     t.integer  "bill_address_id"
     t.datetime "last_ping_time"
