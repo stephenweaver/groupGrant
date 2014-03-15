ae349080beb1524002573363aca63f3fd2637ab5
Stephen J. Weaver <sjwsgm@gmail.com>
2014-03-15 00:31:28 -0500

    facebook fix

--- app/assets/javascripts/facebook_twitter_api.js
+++ app/assets/javascripts/facebook_twitter_api.js
@@ -7,14 +7,13 @@ xfbml      : true
 });
 };
 
-facebook = function () {(function(d, s, id){
-var js, fjs = d.getElementsByTagName(s)[0];
-if (d.getElementById(id)) {return;}
-js = d.createElement(s); js.id = id;
-js.src = "//connect.facebook.net/en_US/all.js";
-fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
-};
+facebook = function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=603004516422329";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk');
 
 // Twitter API setup
 twitter = function() {!function(d,s,id)
@@ -34,11 +33,11 @@ fjs.parentNode.insertBefore(js,fjs);
 
 // Load the buttons
 $(document).ready(function(){
-var href = location.pathname;
-var tweet = "<a href=\'http://www.groupGrant.com" + href;
-    tweet = tweet + "\' class=\'twitter-share-button\' data-lang=\'en\' data-count=\'none\'>Tweet</a>";
-var share = "<div class=\'fb-share-button\' data-href=\'http://www.groupGrant.com" + href + "\'data-width=\'100\' data-type=\'button\'></div>";
-
+var href = document.url;
+var tweet = "<a href='" + href;
+    tweet = tweet + "' class='twitter-share-button' data-lang='en' data-count='none'>Tweet</a>";
+var share = "<div class='fb-share-button' data-href='" + href + "' data-width='100' data-type='button'></div>";
+console.log(share);
 $("#tweets").html(tweet);
 $("#sharePage").html(share);
 });
