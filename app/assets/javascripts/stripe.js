480c5967c7111ccca64237f80c1770a5352a7763
Stephen J. Weaver <sjwsgm@gmail.com>
2014-03-26 08:21:40 -0500

    fix javascript

--- app/assets/javascripts/stripe.js
+++ app/assets/javascripts/stripe.js
@@ -1,38 +1,46 @@
 setup_stripe = function() {
-  if (typeof Stripe !== 'undefined')
+  if($("#payment-form").length > 0)
   {
-    Stripe.setPublishableKey('pk_test_IIvnJ7YTt0yZk0uKrRN5f4Ss');
-    modal();
-  
- }
+
+    
+    if (typeof Stripe !== 'undefined')
+    {
+      console.log("asdf");
+      Stripe.setPublishableKey('pk_test_IIvnJ7YTt0yZk0uKrRN5f4Ss');
+      modal();
+    
+    }
+  }
 }
 
 var stripeResponseHandler = function(status, response) {
-  var $form = $('#payment-form');
+    console.log("test");
+    var $form = $('#payment-form');
 
-  if (response.error && response.error.message.indexOf("An unexpected error has occurred") === -1 ) {
-    // Show the errors on the form
-    $form.find('.payment-errors').text(response.error.message);
-    $form.find('button').prop('disabled', false);
-    return false;
-  } else if (isNaN($('#amount_field').val()) || parseInt($('#amount_field').val()) <= 0 || parseInt($('#amount_field').val()) > 5000) {
-    $form.find('.payment-errors').text("Please enter a positive amount less than 5000");
-    $form.find('button').prop('disabled', false);
-    return false;
-  } 
+    if (response.error && response.error.message.indexOf("An unexpected error has occurred") === -1 ) {
+      // Show the errors on the form
+      $form.find('.payment-errors').text(response.error.message);
+      $form.find('button').prop('disabled', false);
+      return false;
+    } else if (isNaN($('#amount_field').val()) || parseInt($('#amount_field').val()) <= 0 || parseInt($('#amount_field').val()) > 5000) {
+      $form.find('.payment-errors').text("Please enter a positive amount less than 5000");
+      $form.find('button').prop('disabled', false);
+      return false;
+    } 
 
-  else {
-    // token contains id, last4, and card type
-    var token = response.id;
-    var amount = response.amount;
-    // Insert the token into the form so it gets submitted to the server
-    $form.append($('<input type="hidden" name="stripeToken" />').val(token));
-    // and submit
-   $form.get(0).submit();
-  }
+    else {
+      // token contains id, last4, and card type
+      var token = response.id;
+      var amount = response.amount;
+      // Insert the token into the form so it gets submitted to the server
+      $form.append($('<input type="hidden" name="stripeToken" />').val(token));
+      // and submit
+     $form.get(0).submit();
+    }
 };
 
 modal = function(){
+   console.log("test2");
     $('#payment-form').submit(function(event) {
       var $form = $('#payment-form');
       // Disable the submit button to prevent repeated clicks
@@ -41,6 +49,7 @@ modal = function(){
       return false;
     });
 }
+
 
 
 $(window).load(function() {
