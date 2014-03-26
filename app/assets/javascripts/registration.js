b039630230744a33d840fc9c544653d6ef9bc51e
Stephen J. Weaver <sjwsgm@gmail.com>
2014-03-26 08:38:14 -0500

    fix javascript

--- app/assets/javascripts/registration.js
+++ app/assets/javascripts/registration.js
@@ -1,22 +1,32 @@
-$(document).ready(function(){
-  $("#formgroup2").hide();
-   
-  $("#next_button").click(function(){
-  	$("#formgroup1").hide("fast", "swing");
-  	$("#formgroup2").show();
-  });
 
-  $("#previous_button").click(function(){
-  	$("#formgroup1").show("fast", "swing");
-  	$("#formgroup2").hide();
-  });
+
+
+
+
+init_form = function() {
+     $(document).ready(function(){
+     $("#formgroup2").hide();
+      
+     $("#next_button").click(function(){
+      $("#formgroup1").hide("fast", "swing");
+      $("#formgroup2").show();
+     });
+
+     $("#previous_button").click(function(){
+      $("#formgroup1").show("fast", "swing");
+      $("#formgroup2").hide();
+     });
+   });
+
+}
+
+
+
+$(window).load(function() {
+   init_form();
+
 });
-
-
-
-
-
-
+$(document).on('page:load', init_form);
 
 // Work In Progress ---------------------------------------------------------
 // $("#formgroup1").animate({right:'999px'}, 1000).fadeOut("slow").dequeue();
