542028a6503cd3abf1b6a0f18f4b9d8dbf1930b9
Stephen J. Weaver <sjwsgm@gmail.com>
2014-04-03 21:36:13 -0500

    fix production

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,10 +22,6 @@ def store_location
       request.fullpath != Rails.configuration.dumb_url + "/logout" &&
       !request.xhr?) # don't store ajax calls
     session[:previous_url] = request.fullpath
-    Rails.logger.info("store_location")
-    Rails.logger.info(request.fullpath)
-    Rails.logger.info("<%= Rails.configuration.dumb_url %>/users/login")
-    Rails.logger.info(request.fullpath == "<%= Rails.configuration.dumb_url %>/users/login")
   end
 end
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,7 @@ Blankcomposer::Application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   config.facebook_id = 300135003468694;
-  config.dumb_url = "/staging/groupgrant";
+  config.dumb_url = "/expo2014/groupgrant";
 
   Paperclip.options[:command_path] = "/usr/local/bin/"
 end
